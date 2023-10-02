a15a55d59abeffed0634b871c8467e7e966f6525
Maeva Martinez <maevafrancisca@gmail.com>
2023-10-02 15:37:41 -0300

    validaciones registrar

--- src/app/COMPONENTES/registrar/registrar.component.ts
+++ src/app/COMPONENTES/registrar/registrar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http'; // Importa el módulo HttpClient
 import { UsuarioService } from 'src/app/SERVICIOS/usuario.service'; // Importa el servicio UsuarioService
 
@@ -16,17 +16,18 @@ export class RegistrarComponent {
 
   constructor(private usuarioService: UsuarioService){
     this.registroForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      usr_pass: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      confirmPassword: new FormControl('', [Validators.required])
+      email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/), Validators.maxLength(30)]),
+      usr_pass: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(5)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(5)])
     });
   }
+
 
   onSubmit() {
     if (this.registroForm.valid) {
       const email = this.registroForm.get('email')?.value;
       const usr_pass = this.registroForm.get('usr_pass')?.value;
-  
+
       this.usuarioService.registrarUsuario({ email, usr_pass }).subscribe(
         (response) => {
           // Si el registro es exitoso, muestra un mensaje de éxito
@@ -47,5 +48,10 @@ export class RegistrarComponent {
       this.mensajeError = 'Formulario inválido, verifica los campos';
     }
   }
+
 }
+
+
+
+
 
