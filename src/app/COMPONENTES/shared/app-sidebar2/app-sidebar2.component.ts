795eb60dd3d5b7dcf95e5637cf8867341e2e6042
Felipe Duarte <felipe.duarte.acevedo@gmail.com>
2023-10-03 11:30:33 -0300

    acualizacion de navbar

--- src/app/COMPONENTES/shared/app-sidebar2/app-sidebar2.component.ts
+++ src/app/COMPONENTES/shared/app-sidebar2/app-sidebar2.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-app-sidebar2',
   templateUrl: './app-sidebar2.component.html',
   styleUrls: ['./app-sidebar2.component.css']
 })
-export class AppSidebar2Component {}
+export class AppSidebar2Component implements OnInit  {
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+
+  }
+
+  cerrarSesion() {
+    // Simplemente redirige a la página de inicio de sesión
+    this.router.navigate(['/iniciar-sesion']);
+  }
+
+}
