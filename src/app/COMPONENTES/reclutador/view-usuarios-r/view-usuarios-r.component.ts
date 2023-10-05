81f7deb3b5a9a5a974f794e5994a3d9f35c90fbd
Felipe Duarte <felipe.duarte.acevedo@gmail.com>
2023-10-05 16:53:26 -0300

    Angular Material

--- src/app/COMPONENTES/reclutador/view-usuarios-r/view-usuarios-r.component.ts
+++ src/app/COMPONENTES/reclutador/view-usuarios-r/view-usuarios-r.component.ts
@@ -1,23 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/SERVICIOS/usuario.service';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 
 @Component({
   selector: 'app-view-usuarios-r',
   templateUrl: './view-usuarios-r.component.html',
-  styleUrls: ['./view-usuarios-r.component.css']
+  styleUrls: ['./view-usuarios-r.component.css'], 
+ 
 })
-export class ViewUsuariosRComponent implements OnInit {
 
+
+export class ViewUsuariosRComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   usuarios: any[] = [];
-
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor(private usuarioService: UsuarioService,
+    private _liveAnnouncer: LiveAnnouncer,
     private router: Router) { }
 
   ngOnInit(): void {
     this.obtenerUsuarios();
   }
+ 
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
   obtenerUsuarios(): void {
     this.usuarioService.obtenerUsuarios().subscribe(
       (data) => {
@@ -28,5 +64,16 @@ export class ViewUsuariosRComponent implements OnInit {
       }
     );
   }
-
+    /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
