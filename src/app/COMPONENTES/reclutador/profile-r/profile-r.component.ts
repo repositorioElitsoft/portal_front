31ee323e497c70fc50795830030413aa1d7600a5
Felipe Duarte <felipe.duarte.acevedo@gmail.com>
2023-10-16 18:29:54 -0300

    form  perfil

--- src/app/COMPONENTES/reclutador/profile-r/profile-r.component.ts
+++ src/app/COMPONENTES/reclutador/profile-r/profile-r.component.ts
@@ -1,8 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/interface/user.interface';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Pais } from 'src/app/interface/pais.interface';
-import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as intlTelInput from 'intl-tel-input';
 import { NotificationService } from 'src/app/service/notification.service';
 import { UsuarioService } from 'src/app/service/usuario.service';
@@ -14,12 +12,8 @@ import { UsuarioService } from 'src/app/service/usuario.service';
 })
 export class ProfileRComponent implements OnInit {
   editable: boolean = false;
-  guardadoExitoso: boolean = false; // Nueva variable
   form!: FormGroup;
-
-  countries: Pais[] = [];
   isLoaded: boolean = true;
-
   usuarioGuardado: Usuario = {
     usr_nom: '',
     usr_ap_pat: '',
@@ -28,13 +22,12 @@ export class ProfileRComponent implements OnInit {
     usr_herr: '',
     herr_ver: ''
   };
+  router: any;
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router,
     private usuarioService: UsuarioService,
-    private notification: NotificationService,
-    private route: ActivatedRoute
+    private notification: NotificationService
   ) {
     this.buildForm();
   }
@@ -48,23 +41,8 @@ export class ProfileRComponent implements OnInit {
     });
   }
 
-  // Método para habilitar/deshabilitar la edición
-  toggleEditable() {
-    this.editable = !this.editable;
-    if (this.editable) {
-      this.form.enable();
-    } else {
-      this.form.disable();
-    }
-  }
-
   ngOnInit(): void {
     this.ObtenerUsuarioGuardado();
-  }
-
-  navigateToRoute(route: string) {
-    // Navegamos a la ruta proporcionada
-    this.router.navigate([route]);
   }
 
   ObtenerUsuarioGuardado() {
@@ -94,25 +72,36 @@ export class ProfileRComponent implements OnInit {
     });
   }
 
+  toggleEditable() {
+    this.editable = !this.editable;
+    if (this.editable) {
+      this.form.enable();
+    } else {
+      this.form.disable();
+    }
+  }
+  
+  
+
   async submitForm(event: Event) {
     event.preventDefault();
-  
     const user: Usuario = this.form.value;
-  
+    // Guardar los cambios en el servicio
     try {
+      await this.usuarioService.updateUsuario(user).toPromise();
       const isConfirmed = await this.notification.showNotification(
         "success",
         "Datos guardados",
         "Datos actualizados correctamente"
       );
-  
       if (isConfirmed) {
-        await this.usuarioService.updateUsuario(user).toPromise();
-        this.guardadoExitoso = true; // Marcar como guardado exitoso
-        this.router.navigate(['/reclutador/profile-r']);
+        // Redirigir a la página anterior u otra acción
+        this.toggleEditable(); // Vuelve a deshabilitar la edición
       }
     } catch (error) {
-      this.guardadoExitoso = false; // Marcar como no guardado exitoso
+      // Manejar errores
     }
   }
-}
+  
+
+  }
