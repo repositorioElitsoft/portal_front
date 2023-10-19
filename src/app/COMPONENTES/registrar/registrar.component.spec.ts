43a1cb94fc8d8fb731cd5417446284d74de01c7b
cris <c.alexiscanalesmiranda@gmail.com>
2023-10-19 15:05:49 -0300

    Nuevos test a componentes

--- src/app/COMPONENTES/registrar/registrar.component.spec.ts
+++ src/app/COMPONENTES/registrar/registrar.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistrarComponent } from './registrar.component';
+import { UsuarioService } from 'src/app/service/usuario.service';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/service/notification.service';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('RegistrarComponent', () => {
   let component: RegistrarComponent;
@@ -8,7 +14,9 @@ describe('RegistrarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegistrarComponent ]
+      declarations: [ RegistrarComponent ],
+      imports:[ReactiveFormsModule, HttpClientTestingModule],
+      providers:[UsuarioService, Router, NotificationService,HttpClient],
     })
     .compileComponents();
 
