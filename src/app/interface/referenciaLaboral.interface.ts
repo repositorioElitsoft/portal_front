92c91d0a11bf5c980a00f906193c3eb20076045b
Rodrigo Salinas <rodrigo.salinas@elitsoft-chile.com>
2023-11-09 16:05:28 -0300

    Agregando referencias Academicas

--- src/app/interface/referenciaLaboral.interface.ts
+++ src/app/interface/referenciaLaboral.interface.ts
@@ -1,6 +1,6 @@
 export interface ReferenciaLaboral{
     ref_lab_id? : number
-    ref_lab_email? : string
+    ref_lab_email : string
     ref_lab_emp : string
     ref_lab_nom : string
     ref_lab_tel : string
