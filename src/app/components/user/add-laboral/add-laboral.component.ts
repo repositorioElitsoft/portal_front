eb961dadaff368415f77262e5fbf3b1666e938d0
Cristian Canales <c.alexiscanalesmiranda@gmail.com>
2024-01-03 12:16:29 -0300

    limpieza

--- src/app/components/user/add-laboral/add-laboral.component.ts
+++ src/app/components/user/add-laboral/add-laboral.component.ts
@@ -6,7 +6,6 @@ import { HerramientaData } from 'src/app/interface/herramienta-data.interface';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HerramientasService } from 'src/app/service/herramientas.service';
-
 @Component({
   selector: 'app-add-laboral',
   templateUrl: './add-laboral.component.html',
@@ -24,23 +23,20 @@ export class AddLaboralComponent implements OnInit {
   herramientasDisponibles!: HerramientaData[];
   herrIdList: number[] = [];
   navigateToRoute: any;
-
   constructor(
     private formBuilder: FormBuilder,
     public dialog: MatDialog,
     private route: ActivatedRoute, private router: Router,
     private herramientaService:HerramientasService,
     private laboralService: LaboralService,
-    @Inject(MAT_DIALOG_DATA) public data: any, // Datos pasados al diálogo
+    @Inject(MAT_DIALOG_DATA) public data: any, 
     ) {
     this.today = new Date().toISOString().split('T')[0];
     this.buildForm();
   }
-
   ngOnInit(): void {
     this.obtenerLaboralesGuardados();
   }
-
   private buildForm() {
     this.form = this.formBuilder.group({
       inf_lab_crg_emp: ["", [Validators.required]],
@@ -50,29 +46,21 @@ export class AddLaboralComponent implements OnInit {
       inf_lab_fec_fin: ["", [Validators.required]],
       referenciasLaborales: this.formBuilder.array([])
     });
-
     this.generateHerrForm();
   }
-
   obtenerLaboralesGuardados() {
-    console.log('Obteniendo información laboral por ID...'); // Confirmación de que el método se ha llamado
-
     this.laboralService. obtenerListaLaboralPorUsuario().subscribe(
       (data) => {
-        this.laborales = data; // Coloca el objeto único en un arreglo
-        console.log('Datos de laborales recibidos:', this.laborales); // Registro de datos recibidos
+        this.laborales = data; 
       },
       (error) => {
-        console.error('Error al obtener la información laboral:', error); // Registro de errores
+        console.error('Error al obtener la información laboral:', error); 
       }
     );
   }
-
-
   eliminarLaboral(id: number | undefined | null){
     this.laboralService.eliminarLaboral(id).subscribe({
       next:(res)=>{
-        console.log(res);
         this.obtenerLaboralesGuardados();
       },
       error:(err)=>{
@@ -80,18 +68,12 @@ export class AddLaboralComponent implements OnInit {
       }
     });
   }
-
   goBack() {
-    // Restablecer el formulario a su estado inicial
     this.form.reset();
-
-    // Cerrar todas las ventanas de diálogo abiertas
     this.dialog.closeAll();
   }
-
   editarLaboral(id: number | undefined | null){
     this.id = id;
-
     const laboralToEdit = this.laborales.find(laboral => laboral.inf_lab_id === this.id);
     this.form.patchValue({
       inf_lab_crg_emp: laboralToEdit?.inf_lab_crg_emp,
@@ -99,92 +81,49 @@ export class AddLaboralComponent implements OnInit {
       inf_lab_act: laboralToEdit?.inf_lab_act,
       inf_lab_fec_ini: laboralToEdit?.inf_lab_fec_ini,
       inf_lab_fec_fin: laboralToEdit?.inf_lab_fec_fin,
-
     });
-
-
     this.herrIdList.forEach(herrId =>{
       this.form.get(herrId.toString())?.patchValue(false);
     })
-
     laboralToEdit?.herramientas?.forEach(herr =>{
       if(this.herrIdList.find((herrID) => herrID === herr.herr_usr_id)){
         this.form.get(herr.herr_usr_id.toString())?.patchValue(true);
       }
     })
-
-
-
-
-
     this.creationMode = !this.creationMode;
   }
-
   generateHerrForm(){
-
-
     this.herramientaService.getHerramientasByUserId().subscribe({
       next:(data: HerramientaData[])=>{
-
-        console.log("recieved data herramientas: ",data)
         this.herramientasDisponibles = data;
-
         this.herramientasDisponibles.forEach((herramienta)=>{
-
           let wasCheckedAlready = false
-
-
-
           const newControl = new FormControl(wasCheckedAlready);
-
           if (!herramienta.herr_prd_otro){
-
             this.form.addControl(herramienta.herr_usr_id.toString(), newControl);
-
           }
           else {
             this.form.addControl(herramienta.herr_prd_otro, newControl);
           }
-
           this.herrIdList.push(herramienta.herr_usr_id)
-
-
         })
-
         this.checkboxFormCreated = true;
       },
       error:(err: any)=>{
         console.log(err)
       }
     })
-
   }
-
-
-
-
   redirectTo(){
     this.navigateToRoute('/user/cargo-usuario')
   }
-
-
   submitForm(event: Event) {
-    // Prevenir el comportamiento por defecto del evento submit para evitar que la página se recargue
     event.preventDefault();
-
-    // Comprobar si el formulario es válido
     if (this.form.valid) {
-      console.log("Formulario válido, guardando:", this.form.value);
-
-      // Extraer la información del formulario y preparar el objeto para enviar
       const laboralNueva: Laboral = this.form.value;
-
-      // Preparar el arreglo de herramientas seleccionadas
       let herramientasFinal: HerramientaData[] = [];
       this.herrIdList.forEach(id => {
-        // Verificar si la herramienta fue seleccionada en el formulario
         if (this.form.get(id.toString())?.value === true) {
-          // Crear el objeto HerramientaData
           let herra: HerramientaData = {
             herr_usr_id: id,
             herr_is_cert: false,
@@ -207,25 +146,13 @@ export class AddLaboralComponent implements OnInit {
           herramientasFinal.push(herra);
         }
       });
-
-      // Asignar el arreglo de herramientas al objeto laboral
       laboralNueva.herramientas = herramientasFinal;
-
-      // Imprimir la información laboral completa que se enviará
-      console.log("Información laboral a guardar:", laboralNueva);
-
-      // Llamar al servicio para guardar la información laboral
       this.laboralService.guardarLaboral(laboralNueva, this.id).subscribe(
         (laboralGuardada: Laboral) => {
-          // Si el guardado es exitoso, imprimir el resultado y realizar acciones adicionales si son necesarias
-          console.log('Información laboral guardada:', laboralGuardada);
-          this.creationMode = false; // Por ejemplo, cambiar el modo del formulario a no creación
-
-          // Actualizar la lista de experiencias laborales
+          this.creationMode = false; 
           this.laboralService.obtenerListaLaboralPorUsuario().subscribe({
             next:(data) => {
               this.laborales = data;
-                // Cierra el diálogo después de guardar los cambios
                 this.AddLaboralComponent.emit();
                 this.dialog.closeAll();
             },
@@ -235,27 +162,15 @@ export class AddLaboralComponent implements OnInit {
           });
         },
         (error) => {
-          // Si ocurre un error en el guardado, imprimir el error en la consola
           console.error('Error al guardar la información laboral:', error);
         }
       );
     } else {
-      // Si el formulario no es válido, imprimir los errores del formulario
-      console.log('Cancelado : No se guardó el formulario');
-      // Aquí puedes agregar la lógica para manejar un formulario inválido, como mostrar mensajes de error al usuario
     }
   }
-
-
-
   get referenciasFormArray() {
     return this.form.get('referenciasLaborales') as FormArray;
   }
-
-
-
-
-
   addReferencia() {
     const referenciaFormGroup = this.formBuilder.group({
       ref_lab_nom: [''],
@@ -265,14 +180,7 @@ export class AddLaboralComponent implements OnInit {
     });
     this.referenciasFormArray.push(referenciaFormGroup);
   }
-
   eliminarReferencia(index: number) {
     this.referenciasFormArray.removeAt(index);
   }
-
-
-
-
-
-
 }
--- src/app/components/user/añadir-estudio/añadir-estudio.component.ts
+++ src/app/components/user/añadir-estudio/añadir-estudio.component.ts
@@ -6,29 +6,21 @@ import { Academica } from 'src/app/interface/academica.interface';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-
 @Component({
   selector: 'app-añadir-estudio',
   templateUrl: './añadir-estudio.component.html',
   styleUrls: ['./añadir-estudio.component.css'],
 })
-
 export class AñadirEstudioComponent implements OnInit {
 [x: string]: any;
   @Output() AñadirEstudioComponent: EventEmitter<void> = new EventEmitter<void>();
-
   creationMode: boolean = false;
-
   academicas: Academica[] = [];
   id: number | null | undefined = null;
-
   form!: FormGroup;
   minFecha: string = '';
   today: string;
   navigateToRoute: any;
-
-  
-
   constructor(
     private usuarioService: UsuarioService,
     private formBuilder: FormBuilder,
@@ -36,7 +28,7 @@ export class AñadirEstudioComponent implements OnInit {
     private academicaService: AcademicaService,
     private route: ActivatedRoute,
     private router: Router,
-    @Inject(MAT_DIALOG_DATA) public data: any, // Datos pasados al diálogo
+    @Inject(MAT_DIALOG_DATA) public data: any, 
     ) {
       this.today = new Date().toISOString().split('T')[0];
       this.buildForm();
@@ -45,24 +37,16 @@ export class AñadirEstudioComponent implements OnInit {
       if (this.data && this.data.academica) {
         this.editarAcademica(this.data.academica);
       } else {
-        // Código para el modo de creación
+      
         this.creationMode = true;
       }
-
-    
     }
-    
-  
-
     ngAfterViewInit(): void {
       if (this.data && this.data.academica) {
         Promise.resolve().then(() => this.editarAcademica(this.data.academica));
       }
     }
-
-
   startDate = new Date(2020, 0, 1);
-
   private buildForm(academica: Academica | null = null) {
     this.form = this.formBuilder.group({
       inf_acad_est: [academica ? academica.inf_acad_est : "", [Validators.required]],
@@ -73,25 +57,19 @@ export class AñadirEstudioComponent implements OnInit {
       referenciaAcademicas: this.formBuilder.array([])
     });
   }
-  
-
   redirectTo() {
     this.navigateToRoute('/user/informacion-laboral');
   }
-
 obtenerAcademicasGuardados() {
-  console.log('Obteniendo academicas...');
   this.academicaService.obtenerListaAcademicasPorUsuario().subscribe({
     next: (data) => {
       this.academicas = data;
-      console.log('Academicas obtenidas exitosamente:', this.academicas);
     },
     error: (err) => {
       console.log('Error al obtener academicas:', err);
     },
   });
 }
-
   openDialog(
     enterAnimationDuration: string,
     exitAnimationDuration: string
@@ -102,11 +80,9 @@ obtenerAcademicasGuardados() {
       exitAnimationDuration,
     });
   }
-
   eliminarAcademica(id: number | undefined | null) {
     this.academicaService.eliminarAcademica(id).subscribe({
       next: (res) => {
-        console.log(res);
         this.obtenerAcademicasGuardados();
       },
       error: (err) => {
@@ -116,10 +92,8 @@ obtenerAcademicasGuardados() {
   }
 
   goBack() {
-    // Cierra todas las ventanas de diálogo abiertas
     this.dialog.closeAll();
   }
-
   editarAcademica(academica: Academica) {
     if (academica && academica.inf_acad_id) {
       this.id = academica.inf_acad_id;
@@ -130,16 +104,12 @@ obtenerAcademicasGuardados() {
         inf_acad_fec_ini: academica.inf_acad_fec_ini,
         inf_acad_fec_fin: academica.inf_acad_fec_fin,
       });
-  
       this.creationMode = false;
     } else {
       this.creationMode = true;
       this.form.reset();
     }
   }
-  
-
-
   get referenciaFormArray(){
     return this.form.get('referenciaAcademicas') as FormArray;
   }
@@ -152,45 +122,33 @@ obtenerAcademicasGuardados() {
     });
     this.referenciaFormArray.push(referenciaFormGroup);
   }
-
-  
   eliminarReferencia(index: number) {
     this.referenciaFormArray.removeAt(index);
   }
-
-
-
-
   submitForm(event: Event) {
     event.preventDefault();
-
     if (this.form.valid) {
-      // Formatear las fechas antes de enviar al servidor
       const fechaInicioFormateada = new Date(
         this.form.value.inf_acad_fec_ini
       ).toISOString().split('T')[0];
       const fechaFinFormateada = new Date(
         this.form.value.inf_acad_fec_fin
       ).toISOString().split('T')[0];
-
       const academicaNueva: Academica = {
         ...this.form.value,
         inf_acad_fec_ini: fechaInicioFormateada,
         inf_acad_fec_fin: fechaFinFormateada,
       };
-
       this.academicaService
         .guardarAcademica(academicaNueva, this.id)
         .subscribe(
           (academicaGuardada: Academica) => {
-            console.log('Información académica guardada:', academicaGuardada);
             this.creationMode = false;
             this.academicaService
               .obtenerListaAcademicasPorUsuario()
               .subscribe({
                 next: (data) => {
                   this.academicas = data;
-                  // Cierra el diálogo después de guardar los cambios
                   this.AñadirEstudioComponent.emit();
                   this.dialog.closeAll();
                 },
@@ -201,18 +159,9 @@ obtenerAcademicasGuardados() {
           },
           (error) => {
             console.error('Error al guardar información académica:', error);
-            console.log('Error detallado:', error);
           }
         );
     } else {
-      // Muestra un mensaje de error o realiza alguna otra acción si el formulario no está completo.
-      console.log('Por favor, complete todas las casillas del formulario.');
-
-      // Agregar un registro de los errores en el formulario
-      console.log('Errores en el formulario:', this.form.errors);
-
-      // Agregar un registro del estado del formulario
-      console.log('Estado del formulario:', this.form.value);
     }
   }
 }
